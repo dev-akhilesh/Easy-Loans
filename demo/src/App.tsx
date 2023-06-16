2ddd3110800a3b195e02f5290622d540989b577e
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-06-17 01:00:43 +0530

    day4 responsive done

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import EmiCalculator from "./Pages/EmiCalculator";
 
 function App() {
   return (
     <div className="App">
-
+      <EmiCalculator />
     </div>
   );
 }
