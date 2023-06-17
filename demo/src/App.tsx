256f89c454ec41200d2ae813eca530f7a15b1c08
Your Name <you@example.com>
2023-06-18 00:02:01 +0530

    Day-5 work added

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./Pages/login-signup/login";
+import HomePage from "./Pages/Homepage/HomePage";
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage />
     </div>
   );
 }
