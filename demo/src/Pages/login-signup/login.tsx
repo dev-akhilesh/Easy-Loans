92c3e3a135b8c226f39d13a7224b6eb399d4b1f2
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2023-06-17 20:40:01 +0530

    Work Done Day-5

--- demo/src/Pages/login-signup/login.tsx
+++ demo/src/Pages/login-signup/login.tsx
@@ -1,86 +1,137 @@
 import { Toast, useToast } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import { getData } from "../../redux/AuthReducer/action";
+import { addUser, getData } from "../../redux/AuthReducer/action";
 import style from "./Login.module.css";
 import imagelogo from "./Utiles/imagelogo.png";
 
 //TS Interface for Login DataTypes
-export interface loginDatatype{
-  email : string,
-  password : number
+export interface loginDatatype {
+  email: string;
+  password: string;
 }
 
 //TS Interface for Register DataTypes
-export interface registerDatatype{
-  name : string,
-  email : string,
-  password : number
+export interface registerDatatype {
+  name: string;
+  email: string;
+  password: string;
+  image: string;
 }
 
 function Login() {
-
   //Chakra Toast
   const toast = useToast();
 
-  //AuthState 
+  //AuthState
   const [isAuth, setAuth] = useState<Boolean>(false);
 
   //State for Login register form Render
   const [state, setstate] = useState<Boolean>(true);
 
   //State for Login Data
-  const [LoginData, setLoginData] = useState<loginDatatype>({email : "",password : 0})
+  const [LoginData, setLoginData] = useState<loginDatatype>({
+    email: "",
+    password: "",
+  });
 
   //State for Register Data
   const [RegisterData, setRegisterData] = useState<registerDatatype>({
-    name : "",
-    email : "",
-    password : 0
-  })
+    name: "",
+    email: "",
+    password: "",
+    image: "",
+  });
 
   //UsersData
   const [users, setUsers] = useState<registerDatatype[]>();
 
-  useEffect(()=>{
-    getData().then((res)=>{
-      setUsers(res)
+  useEffect(() => {
+    getData().then((res) => {
+      setUsers(res);
     });
-  },[])
+  }, []);
 
   //Submit handler for Login form
-  const handleLogin=(e : React.FormEvent<HTMLFormElement>)=>{
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // console.log(isAuth);
-    isAuth===true ? toast({
-      title: 'login successful',
-      description: "Wellcome to the website",
-      status: 'success',
-      duration: 9000,
-      isClosable: true,
-    }):toast({
-      title: 'wrong credentials',
-      description: "Email or Password is Wrong ",
-      status: 'error',
-      duration: 9000,
-      isClosable: true,
-    })
-  }
+    isAuth === true
+      ? toast({
+          title: "login successful",
+          description: "Wellcome to the website",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        })
+      : toast({
+          title: "wrong credentials",
+          description: "Email or Password is Wrong ",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+  };
 
-  useEffect(()=>{
-    users?.forEach((el : registerDatatype)=>{
-      if(el.email === LoginData.email && +el.password === +LoginData.password){
+  useEffect(() => {
+    users?.forEach((el: registerDatatype) => {
+      if (
+        el.email === LoginData.email &&
+        el.password === LoginData.password
+      ) {
         setAuth(true);
       }
-    })
-  },[LoginData]);
-
+    });
+  }, [LoginData]);
 
   //Submit handler for Register form
-  const handleRegister=(e : React.FormEvent<HTMLFormElement> )=>{
+  const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(RegisterData);
-  }
-
+    if (RegisterData.password === "" ) {
+      toast({
+        title: "wrong credentials",
+        description: "Please Enter valid credentials",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }else if(RegisterData.email === ""){
+      toast({
+        title: "wrong credentials",
+        description: "Please Enter valid credentials",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }else if(RegisterData.name === ""){
+      toast({
+        title: "wrong credentials",
+        description: "Please Enter valid credentials",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    } else {
+      addUser(RegisterData)
+        .then((res) => {
+          toast({
+            title: "Account created.",
+            description: "We've created your account for you.",
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          });
+          setstate(true);
+        })
+        .catch((error) => {
+          toast({
+            title: "wrong credentials",
+            description: "Email or Password is Wrong ",
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+          });
+        });
+    }
+  };
 
   return (
     <div className={style.Container}>
@@ -111,7 +162,10 @@ function Login() {
                   className={style.input}
                   type="email"
                   placeholder="Email"
-                  onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setLoginData({...LoginData, email : e.target.value})}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setLoginData({ ...LoginData, email: e.target.value })
+                  }
+                  // value={LoginData.email}
                 />
               </div>
               <div className={style.inpcont}>
@@ -122,16 +176,18 @@ function Login() {
                   className={style.input}
                   type="password"
                   placeholder="Password"
-                  onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setLoginData({...LoginData, password : +e.target.value})}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setLoginData({ ...LoginData, password: e.target.value })
+                  }
+                  // value={LoginData.password}
                 />
               </div>
               <p className={style.formchange} onClick={() => setstate(false)}>
                 New User?
               </p>
-              <button
-                className={style.submitbtn}
-                type="submit"
-              >Submit</button>
+              <button className={style.submitbtn} type="submit">
+                Submit
+              </button>
               <div className={style.animated2}>
                 <div className={style.line}>Get Ready</div>
                 <div className={style.line}>Easy Loan</div>
@@ -152,7 +208,9 @@ function Login() {
                   className={style.input}
                   type="name"
                   placeholder="Name"
-                  onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setRegisterData({...RegisterData,name : e.target.value})}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setRegisterData({ ...RegisterData, name: e.target.value })
+                  }
                 />
               </div>
               <div className={style.inpcont}>
@@ -163,7 +221,9 @@ function Login() {
                   className={style.input}
                   type="email"
                   placeholder="Email"
-                  onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setRegisterData({...RegisterData, email:e.target.value})}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setRegisterData({ ...RegisterData, email: e.target.value })
+                  }
                 />
               </div>
               <div className={style.inpcont}>
@@ -174,16 +234,20 @@ function Login() {
                   className={style.input}
                   type="password"
                   placeholder="Password"
-                  onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setRegisterData({...RegisterData, password : +e.target.value})}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setRegisterData({
+                      ...RegisterData,
+                      password: e.target.value,
+                    })
+                  }
                 />
               </div>
               <p className={style.formchange} onClick={() => setstate(true)}>
                 Allready a User?
               </p>
-              <button
-                className={style.submitbtn}
-                type="submit"
-              >Register</button>
+              <button className={style.submitbtn} type="submit">
+                Register
+              </button>
               <div className={style.animated2}>
                 <div className={style.line}>Get Ready</div>
                 <div className={style.line}>Easy Loan</div>
